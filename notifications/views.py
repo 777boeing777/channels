1a68120079bbe5863d924b1e21f606af0cac4904
danil <mysc1@yandex.ru>
2023-07-03 14:26:23 +0300

    fixes

--- notifications/views.py
+++ notifications/views.py
@@ -1,9 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
 
 
 def index(request):
-    return render(request, 'index.html', {'room_name': 'broadcast'})
-
+    return render(request, 'index.html')
